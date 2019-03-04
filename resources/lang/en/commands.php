9810018bcf430ce85f03dfdda05a612f795396a0
Zane Hooper <zanehoop@gmail.com>
2019-03-03 23:36:18 -0700

    Move write command to exit command

--- resources/lang/en/commands.php
+++ resources/lang/en/commands.php
@@ -14,26 +14,20 @@ return [
         'ip address :gateway :subnet_mask',
         'ip helper 1 :dhcp_ip',
         'exit',
-
-        # Write the config changes to disk
-        'wr me',
     ],
     'set_interface_port_on' => [
         'interface eth :port',
         'no disable',
-        'wr me',
     ],
     'set_interface_port_off' => [
         'interface eth :port',
         'disable',
-        'wr me',
     ],
     'set_interface_port_speed' => [
         # NOTE: Brocade has weird settings for port speed (1gbps-16gbps) - so we just auto negotiate.
         //'portcfgspeed 0',
         'interface eth :port',
         'speed-duplex :speed',
-        'wr me',
     ],
     'interface_wipe' => [
         'vlan :vlan',
@@ -45,10 +39,10 @@ return [
         'no ip address :gateway :subnet_mask',
         'no ip helper 1 :dhcp_ip',
         'exit',
-
-        # Write the config changes to disk
-        'wr me',
     ],
-    'exit_config' => 'exit',
+    'exit_config' => [
+        'wr me',
+        'exit',
+    ],
     'exit_enable' => 'exit',
 ];
